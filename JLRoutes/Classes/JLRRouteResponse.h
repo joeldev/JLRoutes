40a998d363158d3539aa0c3cd048318a11f3c60a
Joel Levin <joel@joeldev.com>
2017-03-23 20:59:00 -0700

    Doesn't matter, but 2016 -> 2017

--- JLRoutes/Classes/JLRRouteResponse.h
+++ JLRoutes/Classes/JLRRouteResponse.h
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2016, Joel Levin
+ Copyright (c) 2017, Joel Levin
  All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
